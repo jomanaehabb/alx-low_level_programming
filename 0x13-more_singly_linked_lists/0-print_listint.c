406d0d5501a7a026dff9abdeb525fd58f82029c2
jomana <jomanaehab666@yahoo.com>
2023-04-29 23:24:59 -0700

    s

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,13 @@
 #include "lists.h"
 
-
 size_t print_listint(const listint_t *h)
 {
  size_t num = 0;
-
- while (h)
+ while(h)
  {
  printf("%d\n", h->n);
  num++;
- h = h->next;
+ h=h->next;
  }
-
  return (num);
 }
